84d14aa128d7022e1709976dd1f29edfe4bf0352
patrikgrandevall <patrikgrandevall@gmail.com>
2025-04-14 22:02:43 +0200

    fix columns for small device

--- src/app/components/CardSection/CardSection.tsx
+++ src/app/components/CardSection/CardSection.tsx
@@ -29,7 +29,7 @@ const CardSection: React.FC<ICardSection> = ({ games }) => {
           setSearchValue(e.target.value)
         }
       />
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
         {filteredGames.map((game, i) => (
           <motion.div
             key={game.title}
